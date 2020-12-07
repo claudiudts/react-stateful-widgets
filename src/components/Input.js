5fe829c4547ad7c59c2023ac7f74bfea211836a7
Claudiuts <sarb.claudiu391@gmail.com>
2020-12-07 17:41:03 -0600

    input cy test passed

--- src/components/Input.js
+++ src/components/Input.js
@@ -55,13 +55,13 @@ export default function Input() {
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: inputValue.length < 10 ? 'royalblue' : 'crimson'/* STEP 2 */
+    color: (inputValue.length > 10 ? 'crimson' : 'royalblue')/* STEP 2 */
   };
 
   return (
     <div className='widget-input container'>
       <h2>Input</h2>
-      <div id='output' style={style}></div> {/* STEP 3 */}
+      <div id='output' style={style}>{inputValue.toUpperCase()}</div> {/* STEP 3 */}
       <div>
         <input id='input' value={inputValue} type='text' onChange={changeInput} /> {""}
         <button id='resetInput' onClick={reset}>Reset</button>
